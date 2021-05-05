cf3c7915b49c48fd5b1190c2a41ffe7f766739ea
khanhvtn <khanhvotrannhut93@gmail.com>
2021-05-05 15:12:07 +0700

    Finish fix import make style css in wrong position.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Login from './actions/Login/Login';
+import Login from './components/Login/Login';
 
 const App = () => {
     return (
