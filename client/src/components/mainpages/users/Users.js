9e486abd82a5a7713f20c74402c54acee35d3d29
TaiTran <43028616+Benny-tran@users.noreply.github.com>
2021-05-06 14:08:33 +0700

    update

--- client/src/components/mainpages/users/Users.js
+++ client/src/components/mainpages/users/Users.js
@@ -197,19 +197,17 @@ const EnhancedTableToolbar = (props) => {
 
       {numSelected > 0 ? (
         <Tooltip title="Delete">
-          <IconButton aria-label="delete">
-            <Button
-              variant="contained"
-              color="secondary"
-              className={classes.button}
-              startIcon={<DeleteIcon />}
-              onClick={handleDelete}
-            >
-              Delete
-            </Button>
-          </IconButton>
+          <Button
+            variant="contained"
+            color="secondary"
+            className={classes.button}
+            startIcon={<DeleteIcon />}
+            onClick={handleDelete}
+          >
+            Delete
+          </Button>
         </Tooltip>
-        ) : (
+      ) : (
         <Tooltip title="Filter list">
           <IconButton aria-label="filter list">
             <FilterListIcon />
