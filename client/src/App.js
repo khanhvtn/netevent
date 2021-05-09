de2f21bbb3e85a0d14a430e85933f542009e37f9
lamdoan7747 <67705960+lamdoan7747@users.noreply.github.com>
2021-05-09 10:48:10 +0700

    Fix add user button

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Route, Switch, useHistory } from 'react-router-dom';
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import Login from './components/Login/Login';
 import PrivateRoute from './routes/PrivateRoute';
 import PickRole from './components/PickRole/PickRole';
@@ -33,41 +33,42 @@ const App = () => {
                     />
                 </Grid>
             ) : (
-                <Switch>
-                    <Route exact path="/" component={Login} />
-                    <Route exact path="/login" component={Login} />
-                    <PrivateRoute exact path="/pickrole" component={PickRole} />
-                    <Route
-                        exact
-                        path="/confirmation/:id"
-                        component={Confirmation}
-                    />
+                    <Switch>
+                        <Route exact path="/" component={Login} />
+                        <Route exact path="/login" component={Login} />
+                        <PrivateRoute exact path="/pickrole" component={PickRole} />
+                        {/* <Route exact path="/404" component={Error} /> */}
+                        <Route
+                            exact
+                            path="/confirmation/:id"
+                            component={Confirmation}
+                        />
 
-                    {/* Here is the place to add route for dashboard layout */}
-                    <Route>
-                        <Switch>
-                            <Route
-                                path="/dashboard/user"
-                                render={() => (
-                                    <DashboardLayout>
-                                        <Dashboard />
-                                    </DashboardLayout>
-                                )}
-                            />
-                            <Route
-                                path="/dashboard/facility"
-                                render={() => (
-                                    <DashboardLayout>
-                                        <h1>Facility</h1>
-                                    </DashboardLayout>
-                                )}
-                            />
-                            <Route path="*" component={Error} />
-                        </Switch>
-                    </Route>
-                    <Route path="*" component={Error} />
-                </Switch>
-            )}
+                        {/* Here is the place to add route for dashboard layout */}
+                        <Route>
+                            <Switch>
+                                <Route
+                                    path="/dashboard/user"
+                                    render={() => (
+                                        <DashboardLayout>
+                                            <Dashboard />
+                                        </DashboardLayout>
+                                    )}
+                                />
+                                <Route
+                                    path="/dashboard/facility"
+                                    render={() => (
+                                        <DashboardLayout>
+                                            <h1>Facility</h1>
+                                        </DashboardLayout>
+                                    )}
+                                />
+                                <Route path="*" component={Error} />
+                            </Switch>
+                        </Route>
+                        <Route path="*" component={Error} />
+                    </Switch>
+                )}
         </div>
     );
 };
