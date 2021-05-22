4b8cc1ba9fce8515d0655227b16eec0f13a76209
lamdoan7747 <67705960+lamdoan7747@users.noreply.github.com>
2021-05-22 19:57:48 +0700

    Test Github action

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 // const MONGO_URI = `mongodb+srv://khanhvtn93:khanhvtn93123@cluster0.zjom9.mongodb.net/netEvent?authSource=admin&replicaSet=atlas-l3xb7s-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true`;
-const MONGO_URI = `mongodb://localhost:27017`;
+const MONGO_URI = `mongodb://localhost:27017`; 
 mongoose.connect(
     MONGO_URI,
     {
