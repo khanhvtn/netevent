b439778235d579ff30d3b31fcada1903c869646d
TaiTran <43028616+Benny-tran@users.noreply.github.com>
2021-06-05 02:08:58 +0700

    Set up jest, babel, enzyme for React Redux testing

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    setupFiles: ['<rootDir>/src/tests/setup.js']
+    setupFiles: ['<rootDir>/setup.js']
 }
--- client/setup.js
+++ client/setup.js
@@ -1,4 +1,7 @@
 import Enzyme from 'enzyme'
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 
-Enzyme.configure({adapter: new Adapter() })
+Enzyme.configure({
+    adapter: new Adapter,
+    disableLifecycleMethods: true
+})
