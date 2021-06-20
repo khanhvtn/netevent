b0adcfe3f02803127a6888826f2a728060503663
lamdoan7747 <doansonlam7747@gmail.com>
2021-06-20 15:46:50 +0700

    Update new spinner for calendar

--- client/src/components/Calendar/CalendarApp.js
+++ client/src/components/Calendar/CalendarApp.js
@@ -2,20 +2,21 @@ import React, { useEffect, useState } from 'react';
 import { Calendar, Views, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import moment from 'moment';
-import { Paper, Dialog } from '@material-ui/core';
+import { Paper, Dialog, CircularProgress } from '@material-ui/core';
 import CreateEvent from '../CreateEvent/CreateEvent';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { getEvents } from '../../actions/eventActions';
-import { Skeleton } from '@material-ui/lab';
 import CalendarEvent from './CalendarEvent/CalendarEvent';
 import { useHistory } from 'react-router-dom';
 import { getTasks } from '../../actions/taskActions';
+import useStyles from './styles'
 
 const CalendarApp = ({ targetRole }) => {
     const [state, setState] = useState({ open: false, start: null, end: null });
     const dispatch = useDispatch();
     const history = useHistory();
+    const css = useStyles();
     const { events, eventIsLoading, createEventSuccess, userId, tasks, tasksIsLoading } =
         useSelector((state) => ({
             events: state.event.events,
@@ -109,109 +110,107 @@ const CalendarApp = ({ targetRole }) => {
     };
 
     return (
-        <Paper
-            elevation={3}
-            style={{
-                margin: '20px',
-                padding: '20px'
-            }}>
-            {eventIsLoading || tasksIsLoading ? (
+        eventIsLoading || tasksIsLoading ? (
+            <div className={css.circularProgress} align="center">
+                <CircularProgress color="primary" />
+            </div>
+        ) : (
                 <>
-                    <Skeleton height="100px" />
-                    <Skeleton height="100px" />
-                    <Skeleton height="100px" />
-                    <Skeleton height="100px" />
-                    <Skeleton height="100px" />
+                    <Paper
+                        elevation={3}
+                        style={{
+                            margin: '20px',
+                            padding: '20px'
+                        }}>
+                        <Calendar
+                            popup
+                            selectable
+                            formats={{
+                                agendaHeaderFormat: (
+                                    { start, end },
+                                    culture,
+                                    localizer
+                                ) =>
+                                    localizer.format(start, 'DD/MM/YYYY', culture) +
+                                    ' — ' +
+                                    localizer.format(end, 'DD/MM/YYYY', culture)
+                            }}
+                            style={{ height: '100vh' }}
+                            localizer={localizer}
+                            events={genTargetDisplay}
+                            defaultView={Views.MONTH}
+                            scrollToTime={new Date()}
+                            defaultDate={new Date()}
+                            onSelectEvent={handleSelectEvent}
+                            onSelectSlot={handlePickEventTime}
+                            components={{
+                                event: CalendarEvent
+                            }}
+                            eventPropGetter={(date) => {
+                                let styleTemplate = {
+                                    style: {
+                                        color: 'white',
+                                        outline: 'none'
+                                    }
+                                };
+                                const { isApproved, isFinished } = date.resource;
+                                /* Approve Status
+                                Null is pending and color is yellow
+                                True Accepted and color is green
+                                False Rejected and color is red
+                                 */
+                                if (isApproved === null) {
+                                    styleTemplate = {
+                                        ...styleTemplate,
+                                        style: {
+                                            ...styleTemplate.style,
+                                            backgroundColor: `#9e9e9e`
+                                        }
+                                    };
+                                } else if (isFinished === true) {
+                                    styleTemplate = {
+                                        ...styleTemplate,
+                                        style: {
+                                            ...styleTemplate.style,
+                                            backgroundColor: `#4caf50`
+                                        }
+                                    };
+                                } else if (isApproved === true) {
+                                    styleTemplate = {
+                                        ...styleTemplate,
+                                        style: {
+                                            ...styleTemplate.style,
+                                            backgroundColor: `#5c6bc0`
+                                        }
+                                    };
+                                } else {
+                                    styleTemplate = {
+                                        ...styleTemplate,
+                                        style: {
+                                            ...styleTemplate.style,
+                                            backgroundColor: `#e53935`
+                                        }
+                                    };
+                                }
+                                return styleTemplate;
+                            }}
+                        />
+                    </Paper>
+                    {/* Dialog Create Event */}
+                    <Dialog
+                        fullWidth
+                        maxWidth="lg"
+                        open={state.open}
+                        onClose={handleClose}
+                        aria-labelledby="max-width-dialog-title">
+                        <CreateEvent
+                            startDate={state.start}
+                            endDate={state.end}
+                            handleCloseCreateDialog={handleClose}
+                        />
+                    </Dialog>
                 </>
-            ) : (
-                <Calendar
-                    popup
-                    selectable
-                    formats={{
-                        agendaHeaderFormat: (
-                            { start, end },
-                            culture,
-                            localizer
-                        ) =>
-                            localizer.format(start, 'DD/MM/YYYY', culture) +
-                            ' — ' +
-                            localizer.format(end, 'DD/MM/YYYY', culture)
-                    }}
-                    style={{ height: '100vh' }}
-                    localizer={localizer}
-                    events={genTargetDisplay}
-                    defaultView={Views.MONTH}
-                    scrollToTime={new Date()}
-                    defaultDate={new Date()}
-                    onSelectEvent={handleSelectEvent}
-                    onSelectSlot={handlePickEventTime}
-                    components={{
-                        event: CalendarEvent
-                    }}
-                    eventPropGetter={(date) => {
-                        let styleTemplate = {
-                            style: {
-                                color: 'white',
-                                outline: 'none'
-                            }
-                        };
-                        const { isApproved, isFinished } = date.resource;
-                        /* Approve Status
-                        Null is pending and color is yellow
-                        True Accepted and color is green
-                        False Rejected and color is red
-                         */
-                        if (isApproved === null) {
-                            styleTemplate = {
-                                ...styleTemplate,
-                                style: {
-                                    ...styleTemplate.style,
-                                    backgroundColor: `#9e9e9e`
-                                }
-                            };
-                        } else if (isFinished === true) {
-                            styleTemplate = {
-                                ...styleTemplate,
-                                style: {
-                                    ...styleTemplate.style,
-                                    backgroundColor: `#4caf50`
-                                }
-                            };
-                        } else if (isApproved === true) {
-                            styleTemplate = {
-                                ...styleTemplate,
-                                style: {
-                                    ...styleTemplate.style,
-                                    backgroundColor: `#5c6bc0`
-                                }
-                            };
-                        } else {
-                            styleTemplate = {
-                                ...styleTemplate,
-                                style: {
-                                    ...styleTemplate.style,
-                                    backgroundColor: `#e53935`
-                                }
-                            };
-                        }
-                        return styleTemplate;
-                    }}
-                />
-            )}
-            {/* Dialog Create Event */}
-            <Dialog
-                fullWidth
-                maxWidth="lg"
-                open={state.open}
-                onClose={handleClose}
-                aria-labelledby="max-width-dialog-title">
-                <CreateEvent
-                    startDate={state.start}
-                    endDate={state.end}
-                    handleCloseCreateDialog={handleClose}
-                />
-            </Dialog>
-        </Paper>
+            )
     );
 };
 
--- client/src/components/Calendar/styles.js
+++ client/src/components/Calendar/styles.js
@@ -1,3 +1,9 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles(() => ({}));
+export default makeStyles(() => ({
+    circularProgress: {
+        position: 'absolute',
+        top: '50%',
+        left: '55%'
+    }
+}));
