8d7ff077a83000494da493f70d2c6de24f73d447
Vo Tran Nhut Khanh <leekhanh1993@gmail.com>
2021-07-29 20:00:41 +0700

    merge with develop

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const mongoStore = require('connect-mongo');
 const path = require('path');
 const app = express();
 const {
@@ -23,6 +25,24 @@ const cookieParser = require('cookie-parser');
  *  =====================================
  */
 
+//connect to mongodb
+const port = process.env.PORT || 5000;
+const MONGO_URI =
+    process.env.CONNECTION_URL_HOST || `mongodb://localhost:27017`;
+
+const dbConnection = mongoose
+    .connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    })
+    .then((m) => {
+        console.log(`Connect to DB success`);
+        return m.connection.getClient();
+    })
+    .catch((err) => console.error('Error connecting to db:', err));
+
 //middlewares
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
@@ -33,10 +53,19 @@ app.use(
     })
 );
 app.use(cookieParser());
+//config session
+app.use(
+    session({
+        secret: process.env.SECRET_KEY,
+        resave: false,
+        saveUninitialized: false,
+        store: mongoStore.create({
+            clientPromise: dbConnection
+        })
+    })
+);
 
 //routes
-
-//test route
 app.get('/test', (req, res) => {
     res.status(200).json({ message: 'success' });
 });
@@ -61,29 +90,10 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-//error handler
+//use error handler
 app.use(errorHandler);
 
-const port = process.env.PORT || 5000;
-const MONGO_URI =
-    process.env.CONNECTION_URL_HOST || `mongodb://localhost:27017`;
-mongoose.connect(
-    MONGO_URI,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
-    },
-    (err) => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log(`Connect to DB success`);
-        app.listen(port, () =>
-            console.log(`Server is running on port ${port}`)
-        );
-    }
-);
-
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 module.exports = app;
